a57ed44357eefa6e4281cf356fea114e90a4b2c1
zichenmei <143123810+zichenmei@users.noreply.github.com>
2024-04-04 11:40:52 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,23 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
+    int num=0;
+    for(int i=0;i<myList.size();i++){
+      if(myList.get(i).length()==len){
+        num++;
+      }
+    }return num;
     
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
+    for(int i=0;I<myList.size();i++){
+      if(myList.get(i).length()==len){
+        myList.remove(i);
+        i--;
+      }
+    }
 
   }
 }
